656a55943cf59475444e7a626a37f3364ec4675b
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-08-02 22:38:51 -0700

    filling out the row template

--- js/components/store-card.js
+++ js/components/store-card.js
@@ -8,6 +8,20 @@
         return html`
             <tr>
                 <td>${store.name}</td>
+                <td>${store.cookiesPerHour[0]}</td>
+                <td>${store.cookiesPerHour[1]}</td>
+                <td>${store.cookiesPerHour[2]}</td>
+                <td>${store.cookiesPerHour[3]}</td>
+                <td>${store.cookiesPerHour[4]}</td>
+                <td>${store.cookiesPerHour[5]}</td>
+                <td>${store.cookiesPerHour[6]}</td>
+                <td>${store.cookiesPerHour[7]}</td>
+                <td>${store.cookiesPerHour[8]}</td>
+                <td>${store.cookiesPerHour[9]}</td>
+                <td>${store.cookiesPerHour[10]}</td>
+                <td>${store.cookiesPerHour[11]}</td>
+                <td>${store.cookiesPerHour[12]}</td>
+                <td>${store.totalCookiesSold}</td>
             </tr>
         `;
     };
